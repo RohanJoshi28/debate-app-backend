d52c1da78a924d6b36337f26c0f89b8bde1bd920
RohanJoshi28 <rjoshi6@gmail.com>
2023-12-04 19:58:20 -0500

    solve CORS error

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
+app.app_context().push()
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+CORS(app)
+
+#Define the RequestCount model
+class RequestCount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer, default=0)
+
+db.create_all()
+
+if RequestCount.query.get(1) is None:
+    new_counter = RequestCount(id=1, count=0)
+    db.session.add(new_counter)
+    db.session.commit()
+
+#Route to increment request count
+@app.route('/increment', methods=['POST'])
+def increment():
+    counter = RequestCount.query.get(1)
+    if counter:
+        counter.count += 1
+        db.session.commit()
+    return jsonify({"message": "Request count incremented", "currentCount": counter.count}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
