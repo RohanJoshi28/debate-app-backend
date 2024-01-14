6f29d31f562f3846d29a05ecc8059c1bd92611d1
Joshua Kim <joskim24@bergen.org>
2024-01-13 23:34:12 -0500

    Handling some auth in the backend

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, jsonify, request
+import os
+
+import requests
+from flask import Flask, jsonify, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 app.app_context().push()
+CORS(app, origins=['http://localhost:3000'], supports_credentials=True)
 
-CORS(app)
+
+load_dotenv()
+
+app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+jwt = JWTManager(app)
+
+GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
+
+
 
 #Define the RequestCount model
 class RequestCount(db.Model):
@@ -26,6 +42,62 @@ if RequestCount.query.get(1) is None:
     new_counter = RequestCount(id=1, count=0)
     db.session.add(new_counter)
     db.session.commit()
+
+# @app.before_request
+# def handle_preflight():
+#     if request.method == "OPTIONS":
+#         res = Response()
+#         res.headers['Access-Control-Allow-Credentials'] = 'true'
+#         return res
+    
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "hello world"
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth_code = request.get_json()['code']
+
+    data = {
+        'code': auth_code,
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET, 
+        'redirect_uri': 'postmessage',
+        'grant_type': 'authorization_code'
+    }
+
+    response = requests.post('https://oauth2.googleapis.com/token', data=data).json()
+    headers = {
+        'Authorization': f'Bearer {response["access_token"]}'
+    }
+    user_info = requests.get('https://www.googleapis.com/oauth2/v3/userinfo', headers=headers).json()
+
+    #check if user is in database, if not add the user 
+    #FYI FOR LATER ON: MAYBE ONLY ALLOW LOGIN IF USER IS AN EXISTING USER; BC ONLY ADMINS CAN ACCESS
+    # user = User.query.filter_by(email=user_info['email']).first()
+    # if user == None:
+    #     user = User(user_info['name'], user_info['email'])
+    #     db.session.add(user)
+    #     db.session.commit()
+    # else:
+    #     user.name = user_info['name']
+    #     db.session.commit()
+
+    jwt_token = create_access_token(identity=user_info['email'])  
+    response = jsonify(user=user_info)
+    response.set_cookie('access_token_cookie', value=jwt_token, secure=True)
+    return response, 200
+
+
+        
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    jwt_token = request.cookies.get('access_token_cookie')
+    current_user = get_jwt_identity()
+    response = jsonify(logged_in_as=current_user)
+    return response, 200
+
 
 #Route to increment request count
 @app.route('/increment', methods=['POST'])
