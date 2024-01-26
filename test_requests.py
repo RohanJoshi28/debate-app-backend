cf2f6cbd0051a7890eff5a8552ba81dd8729bb1c
jadenmanuel <jadenmanuel2006@gmail.com>
2024-01-26 10:46:36 -0500

    Practical Auth changes

--- test_requests.py
+++ test_requests.py
@@ -1,3 +1,3 @@
 import requests
 
-requests.post("http://127.0.0.1:5000/save_email", data={"name": "Josh", "email": "abc123@gmail.com"})
+requests.post("http://127.0.0.1:5000/save_email", data={"name": "Jaden", "email": "jadenmanuel2006@gmail.com"})
