dfa91aa1e8be8b5c38c76648e990d7658359b0c1
DanielKrs1 <krstndaniel24@gmail.com>
2024-02-12 08:09:58 -0500

    JV Matchmaker

--- JVMatchmaker.java
+++ JVMatchmaker.java
@@ -1,146 +1,226 @@
 import java.util.*;
 
 public class JVMatchmaker {
-    public static void main(String[] args) {    
-        List<SchoolArg> schoolArgs = new ArrayList<>();
-        schoolArgs.add(new SchoolArg(5, 1));
-        schoolArgs.add(new SchoolArg(3, 2));
-
-        List<School> schools = createSchools(schoolArgs);
-        List<Debater> debaters = createDebaters(schoolArgs, schools);
-        sortDebaters(debaters);
-
-        System.out.println("Debaters:");
-        for (Debater debater : debaters) {
-            System.out.println(debater.name);
-        }
-
-        List<Debater> unmatchedDebaters = new ArrayList<>();
-        List<Match> round1Matches = matchDebaters(debaters, unmatchedDebaters);
-
-        System.out.println("Matches:");
-        for (Match match : round1Matches) {
-            System.out.println(match.toString());
-        }
-
-        System.out.println("\nUnmatched:");
-        for (Debater debater : unmatchedDebaters) {
-            System.out.println(debater.name);
-        }
+    public static void main(String[] args) {
+        int[] d = new int[] { 3, 2, 1 };
+        int[] j = new int[] { 1, 1, 1 };
+        matchmake(d, j);
     }
 
-    public static void getMatchmaking(List<SchoolArg> schoolArgs) {
-        List<School> schools = createSchools(schoolArgs);
-        List<Debater> debaters = createDebaters(schoolArgs, schools);
+    private static void matchmake(int[] debaterCounts, int[] judgeCounts) {
+        List<School> schools = createSchools(debaterCounts.length);
+        List<Debater> debaters = createDebaters(debaterCounts, schools);
+        List<Judge> judges = createJudges(judgeCounts, schools);
 
-        List<Debater> unmatchedDebaters = new ArrayList<>();
-        List<Match> round1Matches = createRound(debaters, unmatchedDebaters);
+        Round round1 = createRound(debaters, judges, null);
+        Collections.reverse(debaters);
+        Collections.reverse(judges);
+        Round round2 = createRound(debaters, judges, round1);
+        
+        //flip to look better
+        //Collections.reverse(round2.matches);
+        //issue: instead of stopping at first valid matchup, search for better
+        
+        System.out.println(round1);
+        System.out.println(round2);
     }
 
-    private static List<School> createSchools(List<SchoolArg> schoolArgs) {
+    private static List<School> createSchools(int count) {
         List<School> schools = new ArrayList<>();
 
-        for (int i = 0; i < schoolArgs.size(); i++) {
+        for (int i = 0; i < count; i++) {
             schools.add(new School(i));
         }
 
         return schools;
     }
 
-    private static List<Debater> createDebaters(List<SchoolArg> schoolArgs, List<School> schools) {
+    private static List<Debater> createDebaters(int[] debaterCounts, List<School> schools) {
         List<Debater> debaters = new ArrayList<>();
 
-        for (int schoolIndex = 0; schoolIndex < schools.size(); schoolIndex++) {
-            int debaterCount = schoolArgs.get(schoolIndex).debaterCount;
-
-            for (int counter = 0; counter < debaterCount; counter++) {
-                debaters.add(new Debater(schools.get(schoolIndex), counter));
+        for (int schoolIndex = 0; schoolIndex < debaterCounts.length; schoolIndex++) {
+            for (int index = 0; index < debaterCounts[schoolIndex]; index++) {
+                debaters.add(new Debater(schools.get(schoolIndex), index));
             }
         }
 
+        debaters.sort(Comparable::compareTo);
         return debaters;
     }
 
-    private static List<Match> createRound(List<Debater> debaters, List<Debater> unmatchedDebaters) {
-        List<Match> matches = new ArrayList<>();
-        sortDebaters(debaters);
-        int index = 0;
+    private static List<Judge> createJudges(int[] judgeCounts, List<School> schools) {
+        List<Judge> judges = new ArrayList<>();
 
-        while (index < debaters.size() - 1) {
-            Debater debater1 = debaters.get(index);
-            Debater debater2 = debaters.get(index + 1);
-            
-            if (debater1.school != debater2.school) {
-                matches.add(new Match(debater1, debater2));
-            } else {
+        for (int schoolIndex = 0; schoolIndex < judgeCounts.length; schoolIndex++) {
+            for (int index = 0; index < judgeCounts[schoolIndex]; index++) {
+                judges.add(new Judge(schools.get(schoolIndex), index));
+            }
+        }
+
+        judges.sort(Comparable::compareTo);
+        return judges;
+    }
+
+    private static Round createRound(List<Debater> debaters, List<Judge> judges, Round lastRound) {
+        List<Match> matches = new ArrayList<>();
+        List<Debater> matchedDebaters = new ArrayList<>();
+        List<Judge> matchedJudges = new ArrayList<>();
+
+        for (int debater1Index = 0; debater1Index < debaters.size(); debater1Index++) {
+            Debater debater1 = debaters.get(debater1Index);
+
+            if (matchedDebaters.contains(debater1)) continue;
+
+            for (int debater2Index = 0; debater2Index < debaters.size(); debater2Index++) {
+                Debater debater2 = debaters.get(debater2Index);
+
+                if (matchedDebaters.contains(debater2)) continue;
+                if (!canDebatersMatch(debater1, debater2, lastRound)) continue;
+
+                Judge judge = getJudge(judges, matchedJudges, debater1, debater2);
+
+                if (judge == null) break;
+
+                matches.add(new Match(debater1, debater2, judge));
+                matchedDebaters.add(debater1);
+                matchedDebaters.add(debater2);
+                matchedJudges.add(judge);
                 break;
             }
+        }
 
-            index += 2;
+        return new Round(matches);
+    }
+
+    private static boolean canDebatersMatch(Debater debater1, Debater debater2, Round lastRound) {
+        if (debater1.school == debater2.school) return false;
+        if (lastRound == null) return true;
+
+        return !lastRound.hasMatchup(debater1, debater2);
+    }
+
+    private static Judge getJudge(List<Judge> judges, List<Judge> matchedJudges, Debater debater1, Debater debater2) {
+        for (Judge judge : judges) {
+            if (matchedJudges.contains(judge)) continue;
+            
+            if (judge.school != debater1.school && judge.school != debater2.school) {
+                return judge;
+            }
         }
         
-        for (int i = index; i < debaters.size(); i++) {
-            unmatchedDebaters.add(debaters.get(i));
+        for (Judge judge : judges) {
+            if (matchedJudges.contains(judge)) {
+                continue;
+            }
+
+            return judge;
         }
 
-        return matches;
+        return null;
     }
 
-    private static void sortDebaters(List<Debater> debaters) {
-        debaters.sort((debater1, debater2) -> {
-            if (debater1.index != debater2.index) {
-                return debater1.index - debater2.index;
+    private static class Round {
+        private List<Match> matches;
+
+        public Round(List<Match> matches) {
+            this.matches = matches;
+        }
+
+        public boolean hasMatchup(Debater debater1, Debater debater2) {
+            for (Match match : matches) {
+                if (match.debater1 == debater1 && match.debater2 == debater2 ||
+                match.debater1 == debater2 && match.debater2 == debater1) return true;
             }
-            
-            return debater1.school.index - debater2.school.index;
-        });
-    }
 
-    private static class Match {
-        public Debater debater1;
-        public Debater debater2;
-
-        public Match(Debater debater1, Debater debater2) {
-            this.debater1 = debater1;
-            this.debater2 = debater2;
+            return false;
         }
 
         @Override
         public String toString() {
-            return "(" + debater1.name + ", " + debater2.name + ")";
+            StringBuilder builder = new StringBuilder();
+            builder.append("Round:\n");
+
+            for (Match match : matches) {
+                builder.append(match + "\n");
+            }
+
+            return builder.toString();
         }
     }
 
-    private static class Debater {
-        public School school;
-        public int index;
-        public String name;
+    private static class Match {
+        private Debater debater1;
+        private Debater debater2;
+        private Judge judge;
 
-        public Debater(School school, int index) {
+        public Match(Debater debater1, Debater debater2, Judge judge) {
+            this.debater1 = debater1;
+            this.debater2 = debater2;
+            this.judge = judge;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + debater1 + ", " + debater2 + ", " + judge + ")";
+        }
+    }
+
+    private abstract static class Person implements Comparable<Person> {
+        public School school;
+        private int index;
+
+        public Person(School school, int index) {
             this.school = school;
             this.index = index;
-            this.name = "" + school.name + index;
+        }
+
+        @Override
+        public int compareTo(Person otherPerson) {
+            if (index != otherPerson.index) {
+                return index - otherPerson.index;
+            }
+
+            return school.compareTo(otherPerson.school);
+        }
+
+        @Override
+        public String toString() {
+            return school.toString() + index;
         }
     }
 
-    private static class School {
-        public int index;
-        public char name;
-        
+    private static class Judge extends Person {
+        public Judge(School school, int index) {
+            super(school, index);
+        }
+
+        @Override
+        public String toString() {
+            return "J" + super.toString();
+        }
+    }
+
+    private static class Debater extends Person {
+        public Debater(School school, int index) {
+            super(school, index);
+        }
+    }
+
+    private static class School implements Comparable<School> {
+        private int index;
 
         public School(int index) {
             this.index = index;
-            name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(index);
         }
-    }
 
-    private static class SchoolArg {
-        public int debaterCount;
-        public int judgeCount;
+        @Override
+        public int compareTo(School otherSchool) {
+            return index - otherSchool.index;
+        }
 
-        public SchoolArg(int debaterCount, int judgeCount) {
-            this.debaterCount = debaterCount;
-            this.judgeCount = judgeCount;
+        @Override
+        public String toString() {
+            return "" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(index);
         }
     }
 }
