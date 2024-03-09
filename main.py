fd8ecb7320fb6292512c997e52df0658537b7014
Joshua Kim <joskim24@bergen.org>
2024-03-08 19:23:58 -0500

    Quick cookie samesite config change

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ app.config["JWT_COOKIE_SECURE"] = True
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_COOKIE_SAMESITE"] = "None"
 jwt = JWTManager(app)
 
 GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
