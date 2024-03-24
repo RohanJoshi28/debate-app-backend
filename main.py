302fbf227612f86a0985fb01c5c10c933e6d4435
Joshua Kim <joskim24@bergen.org>
2024-03-24 18:09:56 -0400

    Add samesite and domain attributes

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ app.config["JWT_COOKIE_SECURE"] = True
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
-# app.config["JWT_COOKIE_SAMESITE"] = "None"
-# app.config["JWT_COOKIE_DOMAIN"] = "rohanjoshi.dev"
+app.config["JWT_COOKIE_SAMESITE"] = "None"
+app.config["JWT_COOKIE_DOMAIN"] = "rohanjoshi.dev"
 jwt = JWTManager(app)
 
 GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
