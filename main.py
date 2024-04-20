560357ecd5679264f3b8ea969e690f6f80ce66c1
Joshua Kim <joskim24@bergen.org>
2024-04-20 13:33:22 -0400

    debug deleteuser

--- main.py
+++ main.py
@@ -513,6 +513,8 @@ def deleteuser():
     admin = isAdmin(decode_token(jwt_token['sub']))
     if not admin:
         return "Unauthorized", 401
+    
+    return "Success", 200
     #
     # jwt_token = request.cookies.get('access_token_cookie')
     # admin = isAdmin(current_user)
